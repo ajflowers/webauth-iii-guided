d6b9b758b704c405e8039d99df4b2c183f5df199
Angela Flowers <me@angela.flowers>
2019-11-13 13:13:11 -0500

    users endpoint validates from token

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,23 +1,24 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
+const jwt = require('jsonwebtoken');  //1; npm i jsonwebtoken
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorization;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  if (token) {
+    const secret = process.env.JWT_SECRET || 'How many roads must a man walk down?';
+
+    //check that the token is valid
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if(err) {
+        //bad panda, token tampered with
+        res.status(401).json({ message: 'invalid credentials!' })
+      } else {
+        req.decodedJwt = decodedToken;
+        // console.log(decodedToken);
+        next();
+      }
+    });
+
+
   } else {
     res.status(400).json({ message: 'No credentials provided' });
   }
--- users/users-router.js
+++ users/users-router.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
 
-router.get('/', restricted, (req, res) => {
+router.get('/', restricted, checkRole(["student", "admin"]), (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -12,3 +12,22 @@ router.get('/', restricted, (req, res) => {
 });
 
 module.exports = router;
+
+function checkRole(roles) {
+  return function(req, res, next) {
+    if(roles.includes(req.decodedJwt.role)) {
+      next();
+    } else {
+      res.status(403).json({ message: "Can't touch this!" });
+    }
+  }
+}
+
+
+//data model considerations
+// users
+// roles
+// can a user have more than one role
+// can a role be assigned to more than one user
+// example:
+//   select * from users join roles join ... where user.username = username
